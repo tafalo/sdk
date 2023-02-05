a3f3aa22000d4abcb398fb507f342edf52d78e55
Do Vui <dovui@Dos-MacBook-Pro-2.local>
2023-02-05 20:43:26 +0700

    init a

--- src/tafalo.ts
+++ src/tafalo.ts
@@ -1,111 +1,211 @@
-import { GTM, ResultOk } from "./types";
+import { filterRuleTfl, GTM, payloadType, ResultOk, TypeCookies } from "./types";
 import { getAxiosClient } from "./utils";
 import { AxiosInstance } from "axios";
 export class Tafalo {
-    private appId: string;
-    private tokenId: string;
-    private client: AxiosInstance;
-    private deleted: boolean = false;
-    constructor(_appId: string) {
-        this.appId = _appId;
-        this.client = getAxiosClient("", {});
-        let cc = this.getCookie('token');
-        if (cc != null) {
-            this.tokenId = cc;
-          }
+  private appId: string;
+  private tokenId: string;
+  private client: AxiosInstance;
+  private deleted: boolean = false;
+  private key_store: string = "__key_store_data";
+  private storeCookies: { [key: string]: TypeCookies } = {}
+  constructor(_appId: string) {
+    this.appId = _appId;
+    this.client = getAxiosClient("", {});
+    var lstore = this.strToJson(window.localStorage.get(this.key_store));
+    if (lstore) {
+      this.storeCookies = lstore;
+      this.tokenId = this.cookie("token");
     }
-    
 
-    setCookie(cname: string, cvalue: string, exdays = 0):void {
-        if (cname == 'token') {
-            this.tokenId = cvalue;
-        }
+  }
 
-        window.localStorage.setItem(cname, cvalue)
+  cookie(key: string, value?: any, expire?: number): any {
+    if (typeof key === 'undefined') return null;
+    let keyKey = key.slice(0, key.indexOf("."));
+    let valueKey = "";
+    var _expried = -1;
+    if (keyKey.length < key.length) {
+      valueKey = key.slice(key.indexOf(".") + 1);
     }
-    getCookie(cname: string):string | null {
-        return window.localStorage.getItem(cname);
-    }
-    removeCookie(ckey: string, callback: () => void) {
-        window.localStorage.removeItem(ckey);
-        if (typeof callback == 'function') {
-            callback();
-        }
-    }
-     getTime(str:string, gtm: string):any {
-        try {
-          if (str) {
-            return new Date(str.replace("+00:00", gtm || "+07:00")).getTime();
+
+
+    if (typeof value == 'undefined') {
+      let rsCookies = this.storeCookies[keyKey];
+      if (rsCookies != null && Date.now() - rsCookies.expired > 0) {
+        if (rsCookies.value && valueKey.length > 0) {
+          try {
+            return eval("rsCookies.value." + valueKey);
+          } catch (error) {
+            return null;
           }
-          return '';
-        } catch (ex) {
-          return '';
+
         }
+        return rsCookies.value;
       }
-      
-       strToJson(str:string) {
-        try {
-          if (typeof str == 'string') {
-            return JSON.parse(str);
-          }
-        } catch (error) {
-          return null;
-        }
-      }
-    public async postData(path:string, payload:any):Promise<ResultOk> {
-        try {
-            var rs = await this.client.post(path, payload);
-            var dt = <ResultOk> rs.data;
-            return dt;
-        } catch (error) {
-            return { result: false, timestamp: Date.now()}
-        }
+      return null;
     }
-    public  login(username: string, password: string, callback: (data:ResultOk) => void): void{
-        this.postData("/login", { username: username, password: password }).then(rs => {
-            if(rs.result && rs.data && rs.data.token) {
-                this.setCookie("token", rs.data.token);
-            }
-            callback(rs);
-        });
-        
+    else {
+      if (typeof expire == 'number' && expire > 0) {
+        _expried = Date.now() + expire;
+      }
+      if (value = null || (typeof value == 'string' && value.length == 0)) {
+        delete this.storeCookies[keyKey];
+        window.localStorage.setItem(this.key_store, JSON.stringify(this.storeCookies));
+        return null;
+      }
+
+      this.storeCookies[keyKey] = { value: value, expired: _expried };
+      window.localStorage.setItem(this.key_store, JSON.stringify(this.storeCookies));
+      return value;
     }
 
-    
+
+  }
+
+  getTime(str: string, gtm: string): any {
+    try {
+      if (str) {
+        return new Date(str.replace("+00:00", gtm || "+07:00")).getTime();
+      }
+      return '';
+    } catch (ex) {
+      return '';
+    }
+  }
+
+  strToJson(str: string) {
+    try {
+      if (str === null) return null;
+      if (typeof str == 'string' && str.length > 0) {
+        return JSON.parse(str);
+      }
+    } catch (error) {
+      return null;
+    }
+  }
+  public async postData(path: string, payload: any): Promise<ResultOk> {
+    try {
+      var rs = await this.client.post(path, payload);
+      var dt = <ResultOk>rs.data;
+      return dt;
+    } catch (error) {
+      return { result: false, timestamp: Date.now() }
+    }
+  }
+  public login(username: string, password: string, callback: (data: ResultOk) => void): void {
+    this.postData("/login", { username: username, password: password }).then(rs => {
+      if (rs.result && rs.data && rs.data.token) {
+        this.cookie("token", rs.data.token);
+      }
+      callback(rs);
+    });
+
+  }
+
+
   public logout(callback: (data: ResultOk) => void): void {
+    this.postData("/logout", null).then(rs => {
+      if (rs.result && rs.data && rs.data.token) {
+        this.cookie("token", "");
+      }
+      callback(rs);
+    });
 
   }
-  public createAcount(callback: (data: ResultOk) => void): void {
-    
+  public createAcount(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/create-acount", payload).then(rs => {
+
+      callback(rs);
+    });
+
   }
-  public updateAcount(callback: (data: ResultOk) => void): void {
-    
+  public updateAcount(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/update-acount", payload).then(rs => {
+
+      callback(rs);
+    });
   }
-  public updateApp(callback: (data: ResultOk) => void): void {
-    
+  public removeAcount(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/remove-acount", payload).then(rs => {
+
+      callback(rs);
+    });
   }
-  public removeApp(callback: (data: ResultOk) => void): void {
-    
+
+  public getApp(payload: filterRuleTfl, callback: (data: ResultOk) => void): void {
+    this.postData("/get-app", payload).then(rs => {
+      callback(rs);
+    });
   }
-  public getApp(callback: (data: ResultOk) => void): void {
-    
+  public updateApp(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/update-app", payload).then(rs => {
+
+      callback(rs);
+    });
   }
-  public updateRole(callback: (data: ResultOk) => void): void {
-    
+  public removeApp(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/renove-app", payload).then(rs => {
+
+      callback(rs);
+    });
   }
-  public removeRole(callback: (data: ResultOk) => void): void {
-    
+
+  public updateRole(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/update-role", payload).then(rs => {
+      callback(rs);
+    })
   }
-  public updateUserRole(callback: (data: ResultOk) => void): void {
-    
+  public removeRole(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/remove-role", payload).then(rs => {
+
+      callback(rs);
+    });
   }
-  public updateDocs(callback: (data: ResultOk) => void): void {
-    
+  public updateUserRole(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/update-role", payload).then(rs => {
+      callback(rs);
+    });
   }
-  public removeDocs(callback: (data: ResultOk) => void): void {
-    
+
+  public getFiles(payload: filterRuleTfl, callback: (data: ResultOk) => void): void {
+    this.postData("/get-file", payload).then(rs => {
+      callback(rs);
+    });
   }
-  public getDocs(callback: (data: ResultOk) => void): void {
-    
+  public updateFiles(payload: any, callback: (data: ResultOk) => void): void {
+    this.postData("/update-files", payload).then(rs => {
+      callback(rs);
+    });
   }
+  public uploadFiles(payload: any, callback: (data: ResultOk) => void): void {
+    this.client.post("/upload-files", payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }).then(rs => {
+      callback(rs.data)
+    }).catch(err => {
+      callback({ result: false, msg: err.message, timestamp: Date.now() })
+    });
+
+  }
+  public getDocs(payload: filterRuleTfl | filterRuleTfl[], callback: (data: ResultOk) => void): void {
+    this.postData("/get-docs", payload).then(rs => {
+      callback(rs);
+    });
+  }
+
+  public updateDoc(payload: payloadType, callback: (data: ResultOk) => void): void {
+    this.postData("/update-doc", payload).then(rs => {
+      callback(rs);
+    });
+  }
+
+  public removeDoc(payload: payloadType, callback: (data: ResultOk) => void): void {
+    this.postData("/remove-doc", payload).then(rs => {
+
+      callback(rs);
+    });
+  }
+
 }
--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,62 @@ export enum GTM  {
   "GTM+7" = "+07:00",
   "GTM-7" = "-07:00"
 }
+export type TypeCookies ={
+  value: any,
+  expired: number
+}
+// sum(abc) as t
+export type fnFilterType = {
+  fn: string,
+  args: any[]
+}
+export type fieldFilterType = {
+  fn: string,
+  args: any[],
+  as: string
+}
+export type filterRules = {
+  field:fnFilterType | string | any, 
+  value: any, 
+  op?:string,
+  filterRules?: filterRules[],
+  
+
+}
+export type filterRuleTfl = {
+  fields?: string[] | fieldFilterType[] | any,
+  fieldIgnores?: string[],
+  filterRules?: filterRules[],
+  groupBy?: string[],
+  having?: {type: string, filterRules: {field: fnFilterType | string | any, value: any, op?: string}[]}[],
+  sort?: string,
+  order?: string,
+  page?: number,
+  rows?: number,
+  type?: string,
+  
+
+  t_table?: string
+}
+
+export type payloadType = {
+  _tid: string,
+  _tapp: string,
+  _type: string,
+  _set?: {[key:string]: any},
+  _unset?: string[],
+  _table?: string,
+}
+
+export type appType = {
+  tid: string,
+  type: string,
+  tapp: string,
+  [key:string]: any
+}
+export type accountType = {
+  username: string,
+  password: string,
+  email: string,
+  phone: string
+}
